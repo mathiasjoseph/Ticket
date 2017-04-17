543e0624a1294868065351b203a10f771c109cb0
miky <mathias89100@gmail.com>
2017-04-17 03:14:26 +0200

    Merge branch 'master' of github.com:mikyprog/AdminBundle
    # Veuillez entrer un message de validation pour expliquer en quoi cette fusion est
    # nécessaire, surtout si cela fusionne une branche amont mise à jour dans une branche de sujet.
    #
    # Les lignes commençant par '#' seront ignorées, et un message vide
    # abandonne la validation.

---
+++ Model/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 15/11/16
+ * Time: 00:58
+ */
+
+namespace Miky\Component\Ticket\Model;
+
+
+class Ticket implements TicketInterface
+{
+
+}
